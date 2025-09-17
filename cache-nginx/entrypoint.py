9fa997844dc2816436bd8f5eb790d0497ac65ae5
Ori Hoch <ori@uumpa.com>
2025-09-17 17:11:36 +0300

    cache nginx

--- cache-nginx/entrypoint.py
+++ cache-nginx/entrypoint.py
@@ -3,8 +3,10 @@ import os
 
 
 TYPE = os.environ.get("TYPE", "router")  # router or cache
-NGINX_HTTP_CONFIGS = os.environ.get("NGINX_HTTP_CONFIGS", "")
 NGINX_UPSTREAM_CACHE_SERVERS = os.environ.get("NGINX_UPSTREAM_CACHE_SERVERS", "")
+NGINX_HTTP_CONFIGS = os.environ.get("NGINX_HTTP_CONFIGS", "")
+NGINX_SERVER_CONFIGS = os.environ.get("NGINX_SERVER_CONFIGS", "")
+NGINX_LOCATION_CONFIGS = os.environ.get("NGINX_LOCATION_CONFIGS", "")
 
 
 DEFAULT_CONF_ROUTER_TEMPLATE = '''
@@ -18,8 +20,10 @@ __NGINX_HTTP_CONFIGS__
 server {
     listen       80;
     server_name  _;
+    __NGINX_SERVER_CONFIGS__
     location / {
         proxy_pass http://cache$request_uri;
+        __NGINX_LOCATION_CONFIGS__
     }
 }
 '''
@@ -33,10 +37,10 @@ __NGINX_HTTP_CONFIGS__
 server {
     listen       80;
     server_name  _;
+    __NGINX_SERVER_CONFIGS__
     location / {
         proxy_pass http://tenant.$http_x_cwmcdn_tenant_name.svc.cluster.local$request_uri;
-        proxy_cache_key "$http_x_cwmcdn_tenant_name$request_uri";
-        proxy_cache cwm;
+        __NGINX_LOCATION_CONFIGS__
     }
 }
 '''
@@ -45,6 +49,8 @@ server {
 def main():
     default_conf = DEFAULT_CONF_ROUTER_TEMPLATE if TYPE == "router" else DEFAULT_CONF_CACHE_TEMPLATE
     default_conf = default_conf.replace("__NGINX_HTTP_CONFIGS__", NGINX_HTTP_CONFIGS)
+    default_conf = default_conf.replace("__NGINX_SERVER_CONFIGS__", NGINX_SERVER_CONFIGS)
+    default_conf = default_conf.replace("__NGINX_LOCATION_CONFIGS__", NGINX_LOCATION_CONFIGS)
     if TYPE == "router":
         default_conf = default_conf.replace("__NGINX_UPSTREAM_CACHE_SERVERS__", NGINX_UPSTREAM_CACHE_SERVERS)
     with open("/etc/nginx/conf.d/default.conf", "w") as f:
