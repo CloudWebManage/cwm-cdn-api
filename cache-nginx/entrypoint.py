40d1616d2c68272ce1e34f29f82af513423e6b46
Ori Hoch <ori@uumpa.com>
2025-09-17 16:30:56 +0300

    cache nginx

--- cache-nginx/entrypoint.py
+++ cache-nginx/entrypoint.py
@@ -4,6 +4,10 @@ import os
 
 TYPE = os.environ.get("TYPE", "router")  # router or cache
 NUM_CACHE_SERVERS = int(os.environ.get("NUM_CACHE_SERVERS", "3"))
+if TYPE == "cache":
+    NGINX_HTTP_CONFIGS = os.environ.get("NGINX_HTTP_CONFIGS", "proxy_cache_path /var/cache levels=1:2:3 keys_zone=cwm:1g inactive=5d use_temp_path=off;")
+else:
+    NGINX_HTTP_CONFIGS = os.environ.get("NGINX_HTTP_CONFIGS", "")
 
 
 DEFAULT_CONF_ROUTER_TEMPLATE = '''
@@ -11,6 +15,8 @@ upstream cache {
   hash $http_x_cwmcdn_tenant_name$request_uri consistent;
 __SERVERS__
 }
+
+__NGINX_HTTP_CONFIGS__
 
 server {
     listen       80;
@@ -25,6 +31,8 @@ DEFAULT_CONF_CACHE_TEMPLATE = '''
 resolver 169.254.20.10 valid=30s;
 resolver_timeout 5s;
 
+__NGINX_HTTP_CONFIGS__
+
 server {
     listen       80;
     server_name  _;
@@ -38,14 +46,15 @@ server {
 def main():
     if not TYPE or not NUM_CACHE_SERVERS:
         raise Exception("TYPE, NUM_CACHE_SERVERS environment variables must be set")
+    default_conf = DEFAULT_CONF_ROUTER_TEMPLATE
+    default_conf = default_conf.replace("__NGINX_HTTP_CONFIGS__", NGINX_HTTP_CONFIGS)
     if TYPE == "router":
-        default_conf = DEFAULT_CONF_ROUTER_TEMPLATE
         servers_conf = []
         for i in range(1, NUM_CACHE_SERVERS + 1):
             servers_conf.append(f'  server cache{i}:80;')
         default_conf = default_conf.replace("__SERVERS__", "\n".join(servers_conf))
     elif TYPE == "cache":
-        default_conf = DEFAULT_CONF_CACHE_TEMPLATE
+        pass
     else:
         raise Exception("TYPE must be 'router' or 'cache'")
     with open("/etc/nginx/conf.d/default.conf", "w") as f:
